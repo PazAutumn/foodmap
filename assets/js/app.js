94704d266de1887c055418cf9fd66bfd2f374939
pazautumn <pazcontrerastello@gmail.com>
2017-12-13 13:03:16 -0300

    filtro

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,13 @@ $(document).ready(()=> {
 	}, 5000);
 });
 
+	$("#rest").click(function(){
+		$("#hidden").css("display", "none");
+		for (var i = 0; i < restaurantes.length; i++) {
+			var img = restaurantes[i].image;
+			$("#img"+i).css("background-image", "url("+img+")");
+			$("#img"+i).css("height", "100px");
+			console.log(img);
+		}
+		
+	});
--- assets/js/data.js
+++ assets/js/data.js
@@ -1,3 +1,22 @@
-var restaurantes = [{"name": "El ají seco", "food": "Peruvian", "location": "Santiago", "image" : "http://www.elajiseco.cl/slider/images/elajiseco_mistico/foto01.jpg"},{"name": "Liguria", "food": "Chilean", "location": "Providencia", "image":"https://teatro-nescafe-delasartes.cl/wp-content/uploads/2014/05/liguria-TNA-01.jpg"}, {"name": "Margarita", "food": "Mexican", "location": "Las Condes", "image" : "https://static.vix.com/es/sites/default/files/styles/large/public/imj/4/4-razones-por-las-que-la-comida-mexicana-es-tan-unica-2.jpg?itok=edyVg4U1"}];
+var restaurantes = [
+	{
+		"name": "El ají seco",
+		"food": "Peruvian",
+		"location": "Santiago",
+		"image" : "https://b.zmtcdn.com/data/pictures/4/8300814/26e3bf680da6bb81196e47a8ff8c5d8c.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A"
+		},
+	{
+		"name": "Liguria",
+		"food": "Chilean",
+		"location": "Providencia",
+		"image":"https://teatro-nescafe-delasartes.cl/wp-content/uploads/2014/05/liguria-TNA-01.jpg"
+	},
+	{
+		"name": "Margarita",
+		"food": "Mexican",
+		"location": "Las Condes",
+		"image" : "http://www.24horas.cl/regiones/valparaiso/article1503469.ece/ALTERNATES/w620h350/Vi%C3%B1a%20del%20Mar:%20Fuga%20de%20gas%20genera%20explosi%C3%B3n%20en%20restaurant%20Margarita"
+	}
+];
 
 console.log(restaurantes);
